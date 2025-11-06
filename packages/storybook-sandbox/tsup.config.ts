10d90e792d8a6509101501e9908346c6981128f1
Mellisa Hankins <mellisa.hankins@iress.com>
2025-11-06 17:40:12 +1100

    chore: update React and TypeScript types to version 18.3.1 across multiple packages

--- packages/storybook-sandbox/tsup.config.ts
+++ packages/storybook-sandbox/tsup.config.ts
@@ -87,7 +87,13 @@ export default defineConfig(async (options) => {
       format: ['esm'],
       platform: 'browser',
       target: BROWSER_TARGETS,
-      external: [...globalManagerPackages, ...Object.keys(peerDependencies)],
+      external: [
+        ...globalManagerPackages,
+        ...globalPreviewPackages,
+        '@storybook/react',
+        '@storybook/react-vite',
+        ...Object.keys(peerDependencies),
+      ],
     });
   }
 
