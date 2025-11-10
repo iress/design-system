34a1a0e807cfe8ad952ae4c2799fdd24e077cef3
Mellisa Hankins <mellisa.hankins@iress.com>
2025-11-10 13:04:30 +1100

    feat: add dotenv support for environment variables in Storybook with Okta

--- packages/storybook-okta/.storybook/main.ts
+++ packages/storybook-okta/.storybook/main.ts
@@ -1,5 +1,8 @@
 import { defineMain } from '@storybook/react-vite/node';
 import { autoReloadManagerHead } from '../../../shared/storybook-addon-dev.cjs';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
 
 const config = defineMain({
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -9,8 +12,8 @@ const config = defineMain({
       options: process.env.AUTH
         ? {
             IDS_OKTA: {
-              issuer: 'https://iress.oktapreview.com/oauth2/default',
-              clientId: '0oa1qwfgpgc1JdkWF0h8',
+              issuer: process.env.OKTA_ISSUER,
+              clientId: process.env.OKTA_CLIENT_ID,
             },
           }
         : undefined,
