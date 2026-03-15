128afacfcdcf8dc7e140691bb9bcfe1a154ca473
Mellisa Hankins <mellisa.hankins@iress.com>
2026-03-16 08:40:26 +1100

    chore: update materialSymbols utility to ensure fontWeight is a string and fix fontFeatureSettings formatting

--- packages/theme-preset/src/utilities/materialSymbols.ts
+++ packages/theme-preset/src/utilities/materialSymbols.ts
@@ -3,9 +3,9 @@ import { defineUtility } from '@pandacss/dev';
 
 const baseStyles = {
   fontFamily: MATERIAL_SYMBOLS.family,
-  fontWeight: MATERIAL_SYMBOLS.weight,
+  fontWeight: String(MATERIAL_SYMBOLS.weight),
   verticalAlign: 'middle',
-  fontFeatureSettings: 'liga',
+  fontFeatureSettings: "'liga'",
   display: 'inline-block',
 };
 
