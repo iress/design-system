21017487d8b09cf6a6d6d9bec3059491f610e0b6
Mell <mellisa.hankins@iress.com>
2026-05-08 10:34:12 +1000

    fix(Stack): update margin rule to exclude ids-field-group class (#798)

--- packages/components/src/components/Stack/Stack.styles.ts
+++ packages/components/src/components/Stack/Stack.styles.ts
@@ -4,7 +4,7 @@ export const stack = cva({
   base: {
     display: 'flex',
     flexDirection: 'column',
-    '& > :not(.ids-field)': {
+    '& > :not(.ids-field, .ids-field-group)': {
       marginBlock: 'spacing.0', // If you stick it in a stack, you should be using gap instead of margin to space the items inside the stack
     },
   },
