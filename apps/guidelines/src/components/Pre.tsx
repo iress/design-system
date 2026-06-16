67ea53678354d04147e506253da829b639ff5739
Mellisa Hankins <mellisa.hankins@iress.com>
2026-06-16 13:24:52 +1000

    feat(Pre): enhance code highlighting styles for deleted and inserted tokens

--- apps/guidelines/src/components/Pre.tsx
+++ apps/guidelines/src/components/Pre.tsx
@@ -1,5 +1,6 @@
 import { type HTMLAttributes, useRef, useState } from 'react';
 import { IressButton, IressIcon, IressInline } from '@iress-oss/ids-components';
+import { cssVars } from '@iress-oss/ids-tokens';
 
 export function Pre(props: HTMLAttributes<HTMLPreElement>) {
   const preRef = useRef<HTMLPreElement>(null);
@@ -17,6 +18,25 @@ export function Pre(props: HTMLAttributes<HTMLPreElement>) {
   return (
     <div style={{ position: 'relative' }}>
       <IressInline noWrap bg="colour.neutral.20" mb="spacing.4">
+        <style>{`
+          .token.deleted {
+              color: ${cssVars.colour.system.danger.text};
+          }
+
+          .code-line.deleted {
+              background-color: ${cssVars.colour.system.danger.surface};
+              border-left-color: transparent;
+          }
+
+          .token.inserted {
+              color: ${cssVars.colour.system.success.text};
+          }
+
+          .code-line.inserted {
+              background-color: ${cssVars.colour.system.success.surface};
+              border-left-color: transparent;
+          }
+        `}</style>
       <pre ref={preRef} {...props} style={{ flex: 1, marginBlockEnd: 0 }} />
       {!hideCopy && (
         <IressButton
